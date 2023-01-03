dc21b6c42165015b89b3a974c39791d272fee0a3
John Galt <galt@fedora.com>
2023-01-03 22:48:44 +0100

    e01: empty functions and first test

--- e01-calculate-the-median/src/main.rs
+++ e01-calculate-the-median/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn median(a: Vec<f32>) -> Option<f32> {
+  todo!();
 }
+
+fn main() {
+    let answer: Option<f32> = median(vec![1.0, 2.0, 5.0]);
+
+    println!("median([1.0, 2.0, 5.0]) = {:?}", answer);
+}
+
+#[test]
+fn empty_list() {
+    let input: Vec<f32> = vec![];
+    let expected_output: Option<f32> = None;
+    let actual_output: Option<f32> = median(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+
