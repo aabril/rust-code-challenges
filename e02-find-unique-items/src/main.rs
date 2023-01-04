a14eb4ca10a4145079eae74558e53d3653c42dac
John Galt <galt@fedora.com>
2023-01-04 23:57:59 +0100

    e02: first test passed

--- e02-find-unique-items/src/main.rs
+++ e02-find-unique-items/src/main.rs
@@ -1,3 +1,18 @@
+fn unique(list: Vec<i32>) -> Vec<i32>{
+    list
+}
+
+
 fn main() {
+    let answer: Vec<i32> = unique(vec![1,4,5]);
     println!("Hello, world!");
+    println!("unique([1,4,5]) = {:?}", answer);
+}
+
+#[test]
+fn no_duplicates() {
+    let input = vec![1,4,5];
+    let expected_output = vec![1,4,5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
 }
