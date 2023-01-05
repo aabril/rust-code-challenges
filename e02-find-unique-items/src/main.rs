ad66eccad97ba0b84049e42effe12f825ac50461
John Galt <galt@fedora.com>
2023-01-05 02:29:16 +0100

    * add .gitignore * add tests:   - empty_list   - sorted_list   - sorted_list_with_duplicates   - unsorted_list ( WIP )   - unsorted_list_with_duplicates ( WIP )

--- e02-find-unique-items/src/main.rs
+++ e02-find-unique-items/src/main.rs
@@ -1,17 +1,51 @@
-fn unique(list: Vec<i32>) -> Vec<i32>{
+fn unique(mut list: Vec<i32>) -> Vec<i32>{
+    list.dedup();
     list
 }
 
 
 fn main() {
-    let answer: Vec<i32> = unique(vec![1,4,5]);
-    println!("Hello, world!");
-    println!("unique([1,4,5]) = {:?}", answer);
+    let input: Vec<i32> = vec![2,1,1];
+    let answer: Vec<i32> = unique(input);
+    println!("unique -> {:?}", answer);
 }
 
 #[test]
-fn no_duplicates() {
+fn empty_list() {
+    let input = vec![];
+    let expected_output = vec![];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list() {
     let input = vec![1,4,5];
+    let expected_output = vec![1,4,5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn sorted_list_with_duplicates() {
+    let input = vec![1,4,4,5];
+    let expected_output = vec![1,4,5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+
+#[test]
+fn unsorted_list() {
+    let input = vec![1,5,4];
+    let expected_output = vec![1,4,5];
+    let actual_output = unique(input);
+    assert_eq!(actual_output, expected_output);
+}
+
+#[test]
+fn unsorted_list_with_duplicates() {
+    let input = vec![5,5,4,1];
     let expected_output = vec![1,4,5];
     let actual_output = unique(input);
     assert_eq!(actual_output, expected_output);
