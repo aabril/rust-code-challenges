7be03707112c18731ae81e9e0c40a65e769e9cf3
albert <albert@bigSur.local>
2023-01-05 23:31:54 +0100

    e03: wip -> cstring and path missing

--- e03-print-any-text-type/src/main.rs
+++ e03-print-any-text-type/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::ffi::CString;
+
+fn info<T: std::fmt::Display>(a: &T) {
+    println!("{}", a);
 }
+
+fn main() {
+    let a: &str = "?";
+    let b: String = "?".to_string();
+    info(&a);
+    info(&b);
+}
+
+#[test]
+fn str() {
+    let input: &str = "Rust";
+    info(&input);
+}
+
+#[test]
+fn string() {
+    let input = String::from("Rust");
+    info(&input);
+}
+
+#[test]
+fn chars() {
+    let input = 'r';
+    info(&input);
+}
+
+// #[test]
+// fn cstring() {
+//     use std::ffi::CString;
+//     let input = CString::new("Rust").unwrap();
+//     info(&input);
+// }
+
+// #[test]
+// fn path() {
+//     use std::path::Path;
+//     let input = Path::new("/tmp/rust");
+//     info(input);
+// }
+
+
